59717d2e33458c53f1f3e7e45b220370337d2518
Marco Niederberger <marco.niederberger@adcubum.com>
2020-04-06 16:54:42 +0200

    F Sort Tags

--- app/main/routes.py
+++ app/main/routes.py
@@ -7,7 +7,7 @@ from app.models import Tag
 @bp.route('/', methods=['GET'])
 @bp.route('/index', methods=['GET'])
 def index():
-    return render_template('index.html', title='Home', tags=Tag.query.filter(Tag.path!=None).all())
+    return render_template('index.html', title='Home', tags=Tag.query.filter(Tag.path!=None).order_by(Tag.name).all())
 
 
 @bp.route('/uploads/<filename>')
